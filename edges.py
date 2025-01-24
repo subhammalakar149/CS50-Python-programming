c6f67750d00fd7baee652d96adc4284c5d3425b5
yahngs <subham.malakar149@gmail.com>
2025-01-24 16:23:58 +0530

    Created edges.py

---
+++ edges.py
@@ -0,0 +1,8 @@
+# Finds edges in an image
+
+from PIL import Image, ImageFilter
+
+# Find edges
+before = Image.open("bridge.bmp")
+after = before.filter(ImageFilter.FIND_EDGES)
+after.save("out.bmp")
