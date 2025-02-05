56725f08c0bf9a77ebcb3b99522fda8aa19f26a3
yahngs <subham.malakar149@gmail.com>
2025-02-05 13:02:15 +0530

    Created round2.py

---
+++ round2.py
@@ -0,0 +1,8 @@
+phrase = "You're off to Great Places"
+
+print("Round 2: ", end="")
+for i in range(1, len(phrase) -1):
+    print(phrase[i], end="")
+print("")
+
+# The above code starts at 1st character and subtracts 1 from the length, hence first and the last letter is removed
