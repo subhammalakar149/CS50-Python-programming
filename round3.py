fbe9d7606dff3b576c65055c8f65f2a679c84ca3
yahngs <subham.malakar149@gmail.com>
2025-02-05 13:03:01 +0530

    Created round3.py

---
+++ round3.py
@@ -0,0 +1,8 @@
+phrase = "You're off to Great Places"
+
+print("Round 3: ", end="")
+for character in phrase:
+    print(character, end=" ")
+print("")
+
+# The above code prints out each character of the string and displays it to the user
