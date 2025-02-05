13c96d927f943dc56ea84f95150c2b3c8787ff26
yahngs <subham.malakar149@gmail.com>
2025-02-05 13:03:23 +0530

    Created round4.py

---
+++ round4.py
@@ -0,0 +1,8 @@
+phrase = "You're off to Great Places"
+
+print("Round 4: ", end="")
+for character in phrase[7:]:
+    print(character, end=" ")
+print("")
+
+# The above code displays the user the string with the first 7 characters removed
