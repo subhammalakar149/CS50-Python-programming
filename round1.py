8563dc7a75c8924443955943c4ec2e41ec48053d
yahngs <subham.malakar149@gmail.com>
2025-02-05 13:01:53 +0530

    Created round1.py

---
+++ round1.py
@@ -0,0 +1,8 @@
+phrase = "You're off to Great Places"
+
+print("Round 1: ", end="")
+for i in range(0, len(phrase),2):
+    print(phrase[i], end="")
+print()
+
+# The above code eliminates every 2nd char from the string and prints it to the user
