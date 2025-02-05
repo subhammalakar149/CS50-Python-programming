a071f88bcd11103043ec574ae24039ae943a0c9a
yahngs <subham.malakar149@gmail.com>
2025-02-05 13:03:42 +0530

    Created round5.py

---
+++ round5.py
@@ -0,0 +1,8 @@
+phrase = "You're off to Great Places"
+
+print("Round 5: ", end="")
+for word in phrase.split(" "):
+    print(word)
+print("")
+
+# The above code displays each group of characters separately to the user
